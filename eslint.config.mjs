c39b4f636baead582c45e32852cafe712dce6f05
fronik <askolya23@gmail.com>
2026-02-24 23:16:09 +0200

    setup eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,13 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: [
+      'eslint.config.mjs',
+      'dist/**',
+      'coverage/**',
+      'node_modules/**',
+      '*.min.js',
+    ],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
@@ -30,6 +36,13 @@ export default tseslint.config(
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
       "prettier/prettier": ["error", { endOfLine: "auto" }],
+      'eqeqeq': ['error', 'always'],
+      'no-duplicate-imports': 'error',
+      '@typescript-eslint/no-unsafe-assignment': "warn",
+      '@typescript-eslint/require-await': "warn",
+      '@typescript-eslint/no-unsafe-member-access': "warn",
+      '@typescript-eslint/no-unsafe-return': 'warn',
+      '@typescript-eslint/no-unsafe-call': 'warn',
     },
   },
 );
