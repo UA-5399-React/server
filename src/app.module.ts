c8ef1d409080a33e9d7118a4ce6c213f564e8f63
Gennadiy Greiner <genagreiner94@gmail.com>
2026-02-26 00:28:05 +0200

    feat: setup MongoDB connection via ConfigModule; add env.example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const uri = configService.get<string>('DB_CONNECTION_LINK');
+
+        if (!uri) {
+          throw new Error('DB_CONNECTION_LINK is not defined in the environment variables');
+        }
+
+        return {
+          uri,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
