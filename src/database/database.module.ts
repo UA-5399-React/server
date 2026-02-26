ffd31c3b7fd84b011b9b01169ad77e77852c968f
Gennadiy Greiner <98348568+greiner94@users.noreply.github.com>
2026-02-26 19:52:18 +0200

    Setup MongoDB connection and environment variables (#5)

---
+++ src/database/database.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const uri = configService.get<string>('DB_CONNECTION_LINK');
+        const dbName = configService.get<string>('DB_NAME');
+
+        if (!uri) {
+          throw new Error('DB_CONNECTION_LINK is not defined in the environment variables');
+        }
+
+        if (!dbName) {
+          throw new Error('DB_NAME is not defined');
+        }
+
+        return {
+          uri,
+          dbName,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
