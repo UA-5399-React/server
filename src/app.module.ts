931be09c0801176fbaea1f36af3f050fb9762ee5
Gennadiy Greiner <genagreiner94@gmail.com>
2026-02-26 14:47:09 +0200

    feat: move MongoDB connection setup to DatabaseModule for better organization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -11,27 +11,7 @@ import { AppService } from './app.service';
       envFilePath: '.env',
     }),
 
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => {
-        const uri = configService.get<string>('DB_CONNECTION_LINK');
-        const dbName = configService.get<string>('DB_NAME');
-
-        if (!uri) {
-          throw new Error('DB_CONNECTION_LINK is not defined in the environment variables');
-        }
-
-        if (!dbName) {
-          throw new Error('DB_NAME is not defined');
-        }
-
-        return {
-          uri,
-          dbName,
-        };
-      },
-      inject: [ConfigService],
-    }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
