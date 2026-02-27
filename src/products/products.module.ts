f5273f22772f1458448a81dec492f91ca200c5a3
Gennadiy Greiner <genagreiner94@gmail.com>
2026-02-27 14:09:04 +0200

    feat(48): integrate product schema and update controller/service methods

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { Product, ProductSchema } from './entities/product.schema';
+import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }])],
   providers: [ProductsService],
   exports: [ProductsService],
 })
