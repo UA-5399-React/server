564a9476729b6e710dcb472c0603a2ad54da5ec4
Pavlo Kataranchuk <95435056+PavloJedi@users.noreply.github.com>
2026-02-27 16:51:17 +0200

    feat: enable CORS for client origin (#13)

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Server API')
     .setDescription('API documentation')
