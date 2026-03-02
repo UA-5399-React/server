bdacc14de641a78ca6011963d2e80ca7a3e5b72e
churight <144340708+churight@users.noreply.github.com>
2026-03-02 15:53:20 +0200

    Feat (58): create server ci pipeline (#15)
    * feat(58): add CI pipeline and fix controller tests
    
    * feat(58): test trigger CI
    
    * feat(58): test trigger CI
    
    * feat(58): test trigger CI
    
    * feat(58): test trigger CI
    
    * feat(58): test trigger CI
    
    * feat(58): test trigger CI
    
    * feat(58): changed pnpm setup, testing ci pipeline
    
    * feat(58): changed pnpm setup, testing ci pipeline
    
    * feat(58): cleaned uo

--- src/products/products.controller.spec.ts
+++ src/products/products.controller.spec.ts
@@ -1,12 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
-import { Product } from './entities/product.entity';
 import { ProductStatus } from './enums/product-status.enum';
 
 //moking product for test
-const mockProduct: Product = {
-  id: '1',
+const mockProduct = {
+  _id: '1',
   imageUrl: 'example.com',
   status: ProductStatus.DRAFT,
   title: 'Laptop',
@@ -47,47 +46,47 @@ describe('ProductsControler', () => {
 
   //testing 'findAll', should return an array with only mockProduct inside
   describe('findAll', () => {
-    it('should return all existing products', () => {
+    it('should return all existing products', async () => {
       mockProductsService.findAll.mockReturnValue([mockProduct]);
-      expect(controller.findAll()).toEqual([mockProduct]);
+      expect(await controller.findAll()).toEqual([mockProduct]);
       expect(mockProductsService.findAll).toHaveBeenCalled();
     });
   });
 
   //testing 'findOne', sjould return mockProduct object
   describe('findOne', () => {
-    it('should return 1 product by id', () => {
+    it('should return 1 product by id', async () => {
       mockProductsService.findOne.mockReturnValue(mockProduct);
-      expect(controller.findOne('1')).toEqual(mockProduct);
+      expect(await controller.findOne('1')).toEqual(mockProduct);
       expect(mockProductsService.findOne).toHaveBeenCalledWith('1');
     });
   });
 
   //testing 'create', should create new mockProduct
   describe('create', () => {
-    it('should create a new product', () => {
+    it('should create a new product', async () => {
       mockProductsService.create.mockReturnValue(mockProduct);
-      expect(controller.create(mockProduct)).toEqual(mockProduct);
+      expect(await controller.create(mockProduct)).toEqual(mockProduct);
       expect(mockProductsService.create).toHaveBeenCalledWith(mockProduct);
     });
   });
 
   //testing 'update', should update value 'price' for mockProduct
   describe('update', () => {
-    it('should update product (proce for this case)', () => {
+    it('should update product (proce for this case)', async () => {
       const updateValue = { price: 30000 };
       const updatedProduct = { ...mockProduct, price: 30000 };
       mockProductsService.update.mockReturnValue(updatedProduct);
-      expect(controller.update('1', updateValue)).toEqual(updatedProduct);
+      expect(await controller.update('1', updateValue)).toEqual(updatedProduct);
       expect(mockProductsService.update).toHaveBeenCalledWith('1', updateValue);
     });
   });
 
   //testing 'remove', should remove mockProduct so value will be undefined and message 'Product deleted successfully' is logged
   describe('remove', () => {
-    it('should delete a product and return message', () => {
+    it('should delete a product and return message', async () => {
       mockProductsService.remove.mockReturnValue(undefined);
-      expect(controller.remove('1')).toEqual({
+      expect(await controller.remove('1')).toEqual({
         message: 'Product deleted successfully',
       });
       expect(mockProductsService.remove).toHaveBeenCalledWith('1');
