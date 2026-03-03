c07ada44ceaa5acea2e270ab14ce5ea28cd37168
apoletaev <wild.cat@zoho.com>
2026-03-03 09:25:51 +0200

    feat(62): implement imports sorting (#21)
    Co-authored-by: Oleksii Polietaiev <poletaev.alexei@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
-import { ProductsModule } from './products/products.module';
 import { ProductsController } from './products/products.controller';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { GlobalExceptionFilter } from './filters/global-exception.filter';
 
 async function bootstrap() {
