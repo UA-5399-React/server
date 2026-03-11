e665053351ac4368b08abcde0fbf9838ed701795
churight <144340708+churight@users.noreply.github.com>
2026-03-11 17:46:33 +0200

    Feat(140): filtering by multiple categories (#37)
    * feat(140): added ability to filter by multiple categories
    
    * feat(140): added ability to filter by multiple categories

--- src/products/graphql/products-filter.type.ts
+++ src/products/graphql/products-filter.type.ts
@@ -1,7 +1,7 @@
 import { ProductStatus } from '@/products/enums/product-status.enum';
 
 export type GqlFilters = {
-  category?: string;
+  category?: string[];
   minPrice?: number;
   maxPrice?: number;
   status?: ProductStatus;
