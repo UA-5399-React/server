dbc967101bbf1398181f17869ce8374f6ef54190
pamacat <pavel83838300@gmail.com>
2026-04-27 13:07:15 +0300

    fix(440): restrict reports GraphQL queries to admin roles

--- src/reports/reports.resolver.ts
+++ src/reports/reports.resolver.ts
@@ -1,5 +1,9 @@
+import { UseGuards } from '@nestjs/common';
 import { Args, Query, Resolver } from '@nestjs/graphql';
 
+import { Roles } from '@/auth/decorators/Roles';
+import { GqlAuthGuard } from '@/auth/guards/gql-auth.guard';
+import { RolesGuard } from '@/auth/guards/roles.guard';
 import { GetAbcAnalysisStatisticsArgs } from '@/reports/args/get-abc-analysis-statistics.args';
 import { GetSalesStatisticsArgs } from '@/reports/args/get-sales-statistics.args';
 import { ReportsService } from '@/reports/reports.service';
@@ -7,8 +11,11 @@ import { AbcAnalysisResponse } from '@/reports/types/abc-analysis-response.type'
 import { SalesReportByCategoryResponse } from '@/reports/types/grouped-by-category.type';
 import { SalesReportByDayResponse } from '@/reports/types/grouped-by-day.type';
 import { SalesReportByProdResponse } from '@/reports/types/grouped-by-products.type';
+import { Role } from '@/users/enums/role.enum';
 
 @Resolver()
+@UseGuards(GqlAuthGuard, RolesGuard)
+@Roles(Role.ADMIN, Role.SUPER_ADMIN)
 export class ReportsResolver {
   constructor(private readonly reportsService: ReportsService) {}
 
